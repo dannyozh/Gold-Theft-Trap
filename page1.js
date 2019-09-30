071d2a16b000c4144817f649c54f0cdd17933d32
dannyozh <danielongzhihao@gmail.com>
2019-09-30 13:36:50 +0800

    game is working. yay.

---
+++ page1.js
@@ -0,0 +1,170 @@
+console.log("Up and running. Don't give up!");
+
+//global variables, creating player and computer objects
+
+
+var player = {
+    name: playerName,
+    gold: 1,
+    finalEndState: null,
+};
+var computer = {
+    gold: 1,
+    finalEndState: null,
+};
+
+var playerName;
+var playerChoice;
+var computerChoice;
+var playerCount;
+
+// create counter using DOM
+
+
+////buttons & selectors
+
+// var restartEverything = function () {
+//     player.gold = 1;
+//     computer.gold = 1;
+//     console.clear();
+//     playerIsChoosing();
+//     win();
+// };
+
+
+var clearPage = function () {
+    document.getElementById("start").style.display = "none";
+    playerName = prompt("Welcome to Gold Theft Trap. Please enter your name.");
+    document.getElementById("box-1").style.display = "none";
+    createCounters();
+};
+
+document.querySelector("#start").addEventListener('click', clearPage);
+// document.querySelector("#continue").addEventListener('click', playerIsChoosing);
+// document.querySelector("#restart").addEventListener('click', restartEverything);
+
+//winning/losing variations
+var compareResultGold = function () {
+    if (playerChoice === "Gold" && computerChoice === "Gold") {
+        console.log("Both players get 1 Gold");
+        player.gold ++;
+        computer.gold ++;
+            console.log(playerName, "has", player.gold, "gold");
+            console.log("computer has", computer.gold, "gold");
+            // player score
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/gold-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/gold-coin.jpg");
+                // change result
+                document.querySelector("#resultBox").innerHTML = "Computer chose Gold. Both players gained 1 gold coin.";
+
+    } else if (playerChoice === "Gold" && computerChoice === "Theft") {
+            console.log(playerName, "lost one gold to the computer");
+            var computerTheftWin = computer.gold += 1;
+            var playerTheftLoss = player.gold -= 1;
+            console.log(playerName, "has", playerTheftLoss, "gold");
+            console.log("Computer has", computerTheftWin, "gold.");
+                document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+                document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+                // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/gold-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/theft-coin.jpg");
+                // change result
+                document.querySelector("#resultBox").innerHTML = "Computer played Theft." + " " + playerName + " lost one gold coin to the computer.";
+
+
+    } else if (playerChoice === "Gold" && computerChoice === "Trap") {
+        console.log(playerName, "gain 1 Gold");
+        player.gold ++;
+            console.log(playerName, "has", player.gold, "gold");
+            console.log("computer has", computer.gold, "gold.");
+            // change text
+                document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+                document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/gold-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/trap-coin.jpg");
+            // change result
+                document.querySelector("#resultBox").innerHTML = "Computer played Trap, but to no avail." + " " +playerName + " gain 1 gold coin.";
+
+    }
+};
+
+var compareResultTheft = function() {
+    if (playerChoice === "Theft" && computerChoice === "Gold") {
+        var playerTheftWin = player.gold += 1;
+        var computerTheftLoss = computer.gold -= 1;
+            console.log(playerName, "has", playerTheftWin, "gold.");
+            console.log("computer has", computerTheftLoss, "gold.");
+                document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+                document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+                // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/theft-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/gold-coin.jpg");
+                // change result
+                document.querySelector("#resultBox").innerHTML = playerName + " played Theft and stole 1 gold from computer.";
+
+    } else if (playerChoice === "Theft" && computerChoice === "Theft") {
+        console.log("Both players tried to steal, ending up with no gains.");
+        console.log(playerName, "has", player.gold, "gold");
+        console.log("computer has", computer.gold, "gold");
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/theft-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/theft-coin.jpg");
+            // change result
+                document.querySelector("#resultBox").innerHTML = "Both players played Theft, ending up with no gains.";
+
+
+    } else if (playerChoice === "Theft" && computerChoice === "Trap") {
+        console.log("Computer won, ending the game with", computer.gold, "gold.");
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/theft-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/trap-coin.jpg");
+            // change result
+    }
+};
+
+var compareResultTrap = function () {
+    if (playerChoice === "Trap" && computerChoice === "Gold") {
+        console.log("Computer gained one gold", playerName, "got nothing.");
+        computer.gold += 1;
+        console.log(playerName, "has", player.gold, "gold");
+        console.log("computer has", computer.gold, "gold");
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/trap-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/gold-coin.jpg");
+            // change result
+                document.querySelector("#resultBox").innerHTML = playerName + " played Trap against Computer's Gold and gained nothing." + " " + "Computer gained one gold.";
+
+
+    } else if (playerChoice === "Trap" && computerChoice === "Theft") {
+        console.log(playerName, "called the computer's bluff, winning instantly with a total of", player.gold, "gold.");
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/trap-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/theft-coin.jpg");
+            // change result
+
+    } else if (playerChoice === "Trap" && computerChoice === "Trap") {
+        console.log("Both players tried to trap each other. Both made no gains.");
+        console.log(playerName, "has", player.gold, "gold");
+        console.log("computer has", computer.gold, "gold");
+            document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+            document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+            // change image
+                document.querySelector("#starter-image1").setAttribute("src", "imgs/trap-coin.jpg");
+                document.querySelector("#starter-image2").setAttribute("src", "imgs/trap-coin.jpg");
+            // change result
+                document.querySelector("#resultBox").innerHTML = "Both players tried to trap each other. Both made no gains.";
+
+    }
+};
---
+++ page2.js
@@ -0,0 +1,182 @@
+console.log("woo fun animation");
+//basic win conditions
+
+var win = function () {
+    if (player.gold === 5 || computer.gold === 0) {
+        document.querySelector("#resultBox").innerHTML =playerName + " wins!";
+    } else if (computer.gold === 5 || player.gold === 0) {
+        document.querySelector("#resultBox").innerHTML = "Computer wins!";
+    } else if (computer.gold === 5 && player.gold === 5) {
+        document.querySelector("#resultBox").innerHTML = "It's a draw!";
+    } else if (playerChoice === "Theft" && computerChoice === "Trap") {
+        document.querySelector("#resultBox").innerHTML = "Computer used Trap against" + " " + playerName + "'s Theft" + " and won, ending the game with" + " " + computer.gold + " gold.";
+    } else if (playerChoice === "Trap" && computerChoice === "Theft") {
+        document.querySelector("#resultBox").innerHTML = "Computer played Theft." + " " + playerName + " called the computer's bluff, winning instantly with a total of" + " " + player.gold + " gold.";
+    }
+};
+
+
+//create counters
+
+var createCounters = function () {
+
+var pageDiv = document.createElement('div');
+pageDiv.className = "page-div";
+pageDiv.id = "pageDiv";
+//create counters div
+var counterDiv = document.createElement('div');
+counterDiv.className = "counter-boxes";
+counterDiv.id = "counterBoxes";
+
+// create results div
+var resultsDiv = document.createElement('div');
+resultsDiv.className = "result-box";
+resultsDiv.id = "resultBox";
+resultsDiv.innerHTML = "Result will be determined here!";
+
+// create player counter
+
+var playerCount = document.createElement('div');
+playerCount.className = "player-counter";
+playerCount.id = "playerScore";
+
+//create text in player box
+var playerCountText = document.createElement('p');
+playerCountText.className = "player-text";
+playerCountText.id = "playerScoreText";
+playerCountText.innerHTML = playerName + " gold score is " + player.gold + ".";
+
+//create starting img for  player count box
+
+var starterImage1 = document.createElement("img");
+starterImage1.setAttribute("src", "imgs/gold-coin.jpg");
+starterImage1.id = "starter-image1";
+
+var starterImage2 = document.createElement("img");
+starterImage2.setAttribute("src", "imgs/gold-coin.jpg");
+starterImage2.id = "starter-image2";
+
+//created buttons div and individual buttons
+
+var buttonList1 = document.createElement("div");
+buttonList1.className = "player-options";
+
+var goldButton = document.createElement("button");
+goldButton.className = "gold-button";
+goldButton.id = "theGoldButton";
+goldButton.innerText = "Gold";
+
+var theftButton = document.createElement("button");
+theftButton.className = "theft-button";
+theftButton.id = "theTheftButton";
+theftButton.innerText = "Theft";
+
+var trapButton = document.createElement("button");
+trapButton.className = "trap-button";
+trapButton.id = "theTrapButton";
+trapButton.innerText = "Trap";
+
+// create reset button
+var restartButton = document.createElement("button");
+restartButton.className = "restart-button";
+restartButton.id = "theRestartButton";
+restartButton.innerText = "Restart";
+
+// append everything
+
+buttonList1.appendChild(goldButton);
+buttonList1.appendChild(theftButton)
+buttonList1.appendChild(trapButton);
+playerCount.appendChild(playerCountText);
+playerCount.appendChild(starterImage1);
+playerCount.appendChild(buttonList1);
+
+counterDiv.appendChild(playerCount);
+
+//create computer box
+
+var computerCount = document.createElement('div');
+computerCount.className = "computer-counter";
+computerCount.id = "computerScore";
+
+var computerCountChange = document.createElement('p');
+computerCountChange.className = "computer-score";
+computerCountChange.id = "computerLog";
+computerCountChange.innerHTML = "Computer's gold score is " + computer.gold + ".";
+
+computerCount.appendChild(computerCountChange);
+computerCount.appendChild(starterImage2);
+counterDiv.appendChild(computerCount);
+pageDiv.appendChild(restartButton);
+pageDiv.appendChild(resultsDiv);
+pageDiv.appendChild(counterDiv);
+
+//append to body
+
+document.body.appendChild(pageDiv);
+
+//button functions
+playerMakesChoice();
+};
+
+//player choices
+var playerMakesChoice = function () {
+
+var playerChooseGold = function () {
+    playerChoice = "Gold";
+    console.log(playerName, "You chose", playerChoice);
+    computerIsChoosing();
+    compareResultGold();
+    win();
+    };
+        document.getElementById("theGoldButton").addEventListener('click', playerChooseGold);
+
+var playerChooseTheft = function () {
+    playerChoice = "Theft";
+    console.log(playerName, "You chose", playerChoice);
+    computerIsChoosing();
+    compareResultTheft();
+    win();
+    };
+
+        document.getElementById("theTheftButton").addEventListener('click', playerChooseTheft);
+
+var playerChooseTrap = function () {
+    playerChoice = "Trap";
+    console.log(playerName, "You chose", playerChoice);
+    computerIsChoosing();
+    compareResultTrap();
+    win();
+    };
+
+        document.getElementById("theTrapButton").addEventListener('click', playerChooseTrap);
+
+var clearCounter = function () {
+    player.gold = 1;
+    computer.gold = 1;
+    console.clear();
+    document.querySelector("#resultBox").innerHTML = "Result will be determined here!";
+    document.querySelector("#playerScoreText").innerHTML = playerName + " gold score is " + player.gold + ".";
+    document.querySelector("#computerLog").innerHTML = "Computer's gold score is " + computer.gold + ".";
+    document.querySelector("#starter-image1").setAttribute("src", "imgs/gold-coin.jpg");
+    document.querySelector("#starter-image2").setAttribute("src", "imgs/gold-coin.jpg");
+    };
+    document.getElementById("theRestartButton").addEventListener('click', clearCounter);
+
+};
+
+//computerIsChoosing function
+
+var computerIsChoosing = function () {
+    computerChoice = Math.ceil(Math.random() * 10);
+    if (computerChoice < 3.34) {
+        computerChoice = "Gold";
+        console.log("The computer chooses", computerChoice)
+    } else if (computerChoice <= 6.7) {
+        computerChoice = "Theft";
+        console.log("The computer chooses", computerChoice)
+    } else {
+        computerChoice = "Trap";
+        console.log("The computer chooses", computerChoice);
+    }
+};
